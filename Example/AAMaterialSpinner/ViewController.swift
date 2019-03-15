60aaa592451b3e934832ea35295da82bd56c3de6
Ahsan Ali <hafiz.m.ahsan.ali@gmail.com>
2019-03-15 22:18:38 +0500

    Swift 4.2

---
+++ Example/AAMaterialSpinner/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  AAMaterialSpinner
+//
+//  Created by EngrAhsanAli on 03/08/2019.
+//  Copyright (c) 2019 EngrAhsanAli. All rights reserved.
+//
+
+import UIKit
+import AAMaterialSpinner
+
+class ViewController: UIViewController {
+    
+    var spinnerView: AAMaterialSpinner!
+
+    @IBOutlet weak var loadingView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.aa_ms = self.loadingView.addMaterialSpinner()
+        self.aa_ms.colorArray = [.blue, .red, .orange]
+        self.aa_ms.circleLayer.lineWidth = 3.0
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func popup(_ sender: Any) {
+        
+        
+        let vc = aa_vc_material_spinner(size: 100)
+        vc.aa_ms.circleLayer.lineWidth = 2.0
+        vc.aa_ms.circleLayer.strokeColor = UIColor.blue.cgColor
+        aa_present_material_spinner()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.aa_dismiss_material_spinner()
+        }
+    }
+    
+    
+    @IBAction func start(_ sender: Any) {
+        self.aa_ms.beginRefreshing()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        self.aa_ms.endRefreshing()
+
+    }
+}
+
